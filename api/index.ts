c2191229ac203a61b235d137da0b69622cfd8d3b
koperasitokata <koperasitokata@gmail.com>
2026-05-18 12:13:42 +0800

    Add files via upload

--- api/index.ts
+++ api/index.ts
@@ -27,7 +27,7 @@ const getInitialDB = () => ({
 });
 
 // In-memory DB for Vercel (will reset on cold start)
-let memoryDB = getInitialDB();
+const memoryDB = getInitialDB();
 
 app.use(cors());
 app.use(express.json({ limit: '50mb' }));
